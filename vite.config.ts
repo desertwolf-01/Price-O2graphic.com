112cd97f0aa2f0aa13a0223f8deabadb1544a01c
O2graphic.com <stylesowss@gmail.com>
2025-11-07 20:03:17 +0300

    Fix base URL in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/https://github.com/desertwolf-01/Price-O2graphic.com/', // ← هذا هو المفتاح!
+  base: '/Price-O2graphic.com/', // ← هذا هو الصحيح!
 });
