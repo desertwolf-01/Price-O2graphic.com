6a439ffac12084726c7ef38c2c4e5ea633bc64a2
O2graphic.com <stylesowss@gmail.com>
2025-11-07 20:38:06 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Price-O2graphic.com/', // ← هذا هو المفتاح! لا تغيره!
+  base: '/Price-O2graphic.com/', // لا تنسَ هذا!
 });
