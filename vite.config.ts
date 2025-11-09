305ddee1ac89be11fc19920ed6f95070b4648bb4
O2graphic.com <stylesowss@gmail.com>
2025-11-09 16:35:55 +0300

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // Make sure this plugin is installed: npm install -D @vitejs/plugin-react
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()], // Use the react plugin
-  base: '/Price-O2graphic.com/', // Correct base path for GitHub Pages
-});
