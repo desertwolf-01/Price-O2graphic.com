3a2dcc77fbe5d87b7f655b1d1af561234a8caf79
O2graphic.com <stylesowss@gmail.com>
2025-11-10 19:45:08 +0300

    Update EMAILJS_CONFIG with new service and template IDs

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 // config.ts
 export const EMAILJS_CONFIG = {
-  SERVICE_ID: 'YOUR_ACTUAL_SERVICE_ID', // e.g., 'service_smtp'
-  TEMPLATE_ID: 'YOUR_ACTUAL_TEMPLATE_ID_FOR_INFO_EMAIL', // e.g., 'template_notification_to_me'
-  PUBLIC_KEY: 'euA_gPNF71hZu-USr', // This one is updated
+  SERVICE_ID: 'service_...',
+  TEMPLATE_ID: 'template_...',
+  PUBLIC_KEY: 'euA_gPNF71hZu-USr', // <-- هذا صحيح الآن
 };
