86f5afa4ab49b97daf9b52c7ced6ac1ae14294db
O2graphic.com <stylesowss@gmail.com>
2025-11-10 18:07:52 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,25 @@
-export default defineConfig({
-  base: "/Price-O2graphic.com/", // MUST match your repo name
-  plugins: [react()],
-})
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, '.', '');
+    return {
+      server: {
+        port: 3000,
+        host: '0.0.0.0',
+      },
+      plugins: [react()],
+      define: {
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '.'),
+        }
+      },
+      // Add this line for GitHub Pages deployment
+      base: '/Price-O2graphic.com/',
+    };
+});
